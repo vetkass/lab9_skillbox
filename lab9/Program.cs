68743d17809e8272511e0223804373decb54c2db
SvetaGlobal <s.chernova@globalbit.co.il>
2023-05-12 12:34:11 +0200

    initial dev

---
+++ lab9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lab9 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
