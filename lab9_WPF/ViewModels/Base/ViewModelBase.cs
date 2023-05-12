8a8ffc5c07a406e1a88a7ca1c727c886277818ab
SvetaGlobal <s.chernova@globalbit.co.il>
2023-05-12 13:35:09 +0200

    base class of ViewModel

---
+++ lab9_WPF/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace lab9_WPF.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Разрешить кольцевые изменения свойств
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field">поле</param>
+        /// <param name="value">новое значение поля</param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? propertyName=null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+
+        }
+    }
+}
