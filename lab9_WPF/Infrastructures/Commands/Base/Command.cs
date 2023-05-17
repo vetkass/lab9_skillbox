83ce26d14c940d26999d74f59572209788625b5a
SvetaGlobal <s.chernova@globalbit.co.il>
2023-05-17 14:29:44 +0200

    add command CloseApp

---
+++ lab9_WPF/Infrastructures/Commands/Base/Command.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPF_MVVM.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        /// <summary>
+        /// генерируется когда функция выполнения команды начинает возвращать другое значение
+        /// </summary>
+        public event EventHandler? CanExecuteChanged
+        {
+            //данной реализацие мы передаем управление обработки ивентов самому WFP
+            //WPF автоматически генерирует событие для CommandManager, когда что-либо происходит
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /// <summary>
+        /// функция
+        /// если возвр ложь то команду выполнить нельзя, и элемент будет отключаться
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns>истина или ложь</returns>
+        public abstract bool CanExecute(object? parameter);
+
+        /// <summary>
+        /// основная логика команды, то что дб выполнено
+        /// </summary>
+        /// <param name="parameter"></param>
+        public abstract void Execute(object? parameter);
+    }
+}
---
+++ lab9_WPF/Infrastructures/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Windows;
+using WPF_MVVM.Infrastructure.Commands.Base;
+
+namespace lab9_WPF.Infrastructures.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
